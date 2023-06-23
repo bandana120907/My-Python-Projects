2826d4b2456817f78ff16c942a02d41e6b94e743
bandana120907 <92863250+bandana120907@users.noreply.github.com>
2023-06-24 01:29:35 +0530

    Update stalkmycoding.py

--- stalkmycoding.py
+++ stalkmycoding.py
@@ -1,5 +1,15 @@
 import webbrowser
-c=str(input('codechef: '))
+
+c=str(input("Enter a username: "))
+while ' ' in c:
+    print('enter valid usernames without spaces!')
+    c = str(input("Enter a username: "))
+
+d={insta:'https://instagram.com/'+c}
+for i in d:
+    webbrowser.open(d.get(i))
+    
+'''c=str(input('codechef: '))
 g=str(input('geeksforgeeks id: '))
 while ' ' in c or ' ' in g:
     print('enter valid usernames without spaces!')
@@ -8,4 +18,4 @@ while ' ' in c or ' ' in g:
 cdc='https://codechef.com/users/'+c
 gfg='https://auth.geeksforgeeks.org/user/'+g+'/practice/'
 webbrowser.open(cdc)
-webbrowser.open(gfg)
+webbrowser.open(gfg)'''
